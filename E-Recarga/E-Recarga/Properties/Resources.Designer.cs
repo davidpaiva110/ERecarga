b74a149870858f712ebe1490c5b83a199e982158
rafaelsa99 <rafa.sa_@hotmail.com>
2019-11-10 18:50:35 +0000

    layout index e contactos

--- E-Recarga/E-Recarga/Properties/Resources.Designer.cs
+++ E-Recarga/E-Recarga/Properties/Resources.Designer.cs
@@ -63,19 +63,9 @@ namespace E_Recarga.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap slider1 {
+        internal static System.Drawing.Bitmap background {
             get {
-                object obj = ResourceManager.GetObject("slider1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap slider2 {
-            get {
-                object obj = ResourceManager.GetObject("slider2", resourceCulture);
+                object obj = ResourceManager.GetObject("background", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }

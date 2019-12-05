732b247695b72ebaedc4c45e8992dc841b3f70d5
rafaelsa99 <rafa.sa_@hotmail.com>
2019-12-05 03:24:16 +0000

    Reservas e Histórico de Reservas: tudo feito

--- E-Recarga/E-Recarga/Models/Reserva.cs
+++ E-Recarga/E-Recarga/Models/Reserva.cs
@@ -10,7 +10,7 @@ namespace E_Recarga.Models
     public class Reserva
     {
         [Key]
-        [Display(Name = "ID Estação")]
+        [Display(Name = "ID Reserva")]
         public int ReservaId { get; set; }
         [Required(ErrorMessage = "Tem de especificar a data da reserva!")]
         [Display(Name = "Data")]
@@ -26,11 +26,33 @@ namespace E_Recarga.Models
         public DateTime HoraFim { get; set; }
 
         [ForeignKey("Posto")]
+        [Display(Name = "ID Posto")]
         public int PostoId { get; set; }
         public Posto Posto { get; set; }
 
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
+
+        public Reserva()
+        {
+        }
+
+        public Reserva(int postoId, DateTime data, DateTime horaInicio, DateTime horaFim)
+        {
+            Data = data;
+            HoraInicio = horaInicio;
+            HoraFim = horaFim;
+            PostoId = postoId;
+        }
+
+        public Reserva(DateTime data, DateTime horaInicio, DateTime horaFim, int postoId, string userId)
+        {
+            Data = data;
+            HoraInicio = horaInicio;
+            HoraFim = horaFim;
+            PostoId = postoId;
+            UserId = userId;
+        }
     }
 }

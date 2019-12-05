2c96e665bd238c99e1545b1787c5b5e9d12edadb
rafaelsa99 <rafa.sa_@hotmail.com>
2019-12-05 17:39:24 +0000

    Index e Sobre Nós prontos

--- E-Recarga/E-Recarga/Controllers/HomeController.cs
+++ E-Recarga/E-Recarga/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using E_Recarga.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+
 
 namespace E_Recarga.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,14 +20,21 @@ namespace E_Recarga.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            List<Estacao> estacoesComPostosAtivos = new List<Estacao>();
+            List<Estacao> allEstacoes = new List<Estacao>();
+            var estacoes = db.Estacoes.Include(v => v.RedeProprietaria);
+            foreach (Estacao est in estacoes) allEstacoes.Add(est);
+            foreach (Estacao est in allEstacoes)
+            {
+                var postosAtivos = db.Postos.Where(c => c.EstacaoId == est.EstacaoId && c.Estado == false);
+                if (postosAtivos.Count() > 0)
+                    estacoesComPostosAtivos.Add(est);
+            }
+            return View(estacoesComPostosAtivos);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }

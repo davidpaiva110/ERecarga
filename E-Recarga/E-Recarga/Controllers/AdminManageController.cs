df5ee0cd3543130f397b54172fdcfe8b9c2d9304
davidpaiva110@gmail.com <davidpaiva110@gmail.com>
2019-12-05 00:43:57 +0000

    Administrador: adicionar posto

---
+++ E-Recarga/E-Recarga/Controllers/AdminManageController.cs
@@ -0,0 +1,58 @@
+﻿using E_Recarga.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity; //necessário por causa das querys sql -> para reconhecer as entidades
+using System.Net;
+
+namespace E_Recarga.Controllers
+{
+
+    [Authorize(Roles = "Admin")]
+    public class AdminManageController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AdminManage
+        public ActionResult ListarPostosPendentes()
+        {
+            List<Posto> postosPendentes = new List<Posto>();
+            var postos = db.Postos.Include(p => p.Estacao).Include(p => p.Estacao.RedeProprietaria).Where(p => p.Estado.Equals(false));
+            foreach (Posto posto in postos)
+                postosPendentes.Add(posto);
+            return View(postosPendentes.ToList());
+        }
+
+        public ActionResult AceitarPostoAdmin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posto posto = db.Postos.Include(r => r.Estacao).Include(r => r.Estacao.RedeProprietaria).SingleOrDefault(r => r.PostoId == id);
+            if (posto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AceitarPostoAdmin([Bind(Include = "PostoId,EstacaoId")] Posto posto)
+        {
+            if (ModelState.IsValid)
+            {
+                posto.Estado = true;
+                db.Entry(posto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListarPostosPendentes");
+            }
+            return View(posto);
+        }
+
+
+    }
+}
